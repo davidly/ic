3f4dbed805abdf63aae81ac6d322852137994bcf
David Lee <davidly_lee@hotmail.com>
2021-12-20 13:30:19 -0800

    Update m.bat

--- m.bat
+++ m.bat
@@ -4,7 +4,7 @@ del ic.exe
 del ic.pdb
 @echo on
 
-cl /nologo ic.cxx /Ox /Qpar /O2 /Oi /Ob2 /EHac /Zi /Gy /DNDEBUG /DUNICODE /D_AMD64_ /link ntdll.lib /OPT:REF
-REM cl /nologo ic.cxx /Ox /Qpar /O2 /Oi /Ob2 /EHac /Zi /Gy /DUNICODE /D_AMD64_ /link ntdll.lib /OPT:REF
+cl /nologo ic.cxx /I.\ /Ox /Qpar /O2 /Oi /Ob2 /EHac /Zi /Gy /DNDEBUG /DUNICODE /D_AMD64_ /link ntdll.lib /OPT:REF
+REM cl /nologo ic.cxx /I.\ /Ox /Qpar /O2 /Oi /Ob2 /EHac /Zi /Gy /DUNICODE /D_AMD64_ /link ntdll.lib /OPT:REF
 
 
